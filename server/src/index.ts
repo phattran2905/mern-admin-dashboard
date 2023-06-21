c68c40568de8395b84e7984e7888c0d5e3cb2b80
phat@minim1 <phat.tranducvn@gmail.com>
2023-06-20 18:18:16 -0700

    Insert data user

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import { config } from "dotenv";
 import mongoose from "mongoose";
 import app from "./app";
+import UserModel from "./models/User.model";
+import { dataUser } from "./data";
 
 config();
 
@@ -9,5 +11,7 @@ mongoose
 	.connect(process.env.MONGODB_DB_URL || "mongodb://localhost:27017")
 	.then(() => {
 		app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+		// UserModel.insertMany(dataUser);
 	})
 	.catch((error) => console.log(`${error} did not connect.`));
