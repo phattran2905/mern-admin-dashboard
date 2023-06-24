c1c824e570426c18c743e0beb2421aefacf9741d
phat@minim1 <phat.tranducvn@gmail.com>
2023-06-23 18:26:35 -0700

    Fetch API /customers

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_APP_BASE_URL }),
 	reducerPath: "adminApi",
-	tagTypes: ["User", "Products"],
+	tagTypes: ["User", "Products", "Customers"],
 	endpoints: (build) => ({
 		getUser: build.query<UserResponse, string>({
 			query: (id: string) => `general/user/${id}`,
@@ -15,7 +15,11 @@ export const api = createApi({
 			query: () => `client/products`,
 			providesTags: ["Products"],
 		}),
+		getCustomers: build.query<UserResponse[], void>({
+			query: () => `client/customers`,
+			providesTags: ["Customers"],
+		}),
 	}),
 });
 
-export const { useGetUserQuery, useGetProductsQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery } = api;
