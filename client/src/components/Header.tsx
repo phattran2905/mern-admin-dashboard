3e8240a5cc6b696b4b94f48785f11e40904d8b80
phat@minim1 <phat.tranducvn@gmail.com>
2023-06-23 17:32:55 -0700

    Add component Header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,29 @@
+import { Typography, Box, useTheme } from "@mui/material";
+
+type Props = {
+	title: string;
+	subtitle: string;
+};
+function Header({ title, subtitle }: Props) {
+	const theme = useTheme();
+
+	return (
+		<Box>
+			<Typography
+				variant="h2"
+				color={theme.palette.secondary[100]}
+				fontWeight={"bold"}
+				sx={{ mb: "5px" }}
+			>
+				{title}
+			</Typography>
+			<Typography
+				variant="h5"
+				color={theme.palette.secondary[300]}
+			>
+				{subtitle}
+			</Typography>
+		</Box>
+	);
+}
+export default Header;
